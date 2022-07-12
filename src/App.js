bec575e58bca86cca6c8b40e971e04dab10db99e
emailrk24 <109119732+emailrk24@users.noreply.github.com>
2022-07-12 07:40:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import BookingEntry from "./components/BookingEntry";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello there...</h1>
+      <BookingEntry />
+    </div>
+  );
+}
---
+++ src/components/BookingEntry.js
@@ -0,0 +1,27 @@
+import "./BookingEntry.css";
+
+export default function BookingEntry() {
+  return (
+    <div>
+      <h2> Booking Entry...</h2>
+      <h3>Checkin date</h3>
+      <h3>Checkout date</h3>
+      <h3>Source</h3>
+      <h3>Booking Id</h3>
+      <h3>Room no.</h3>
+      <h3>No. of rooms</h3>
+      <h3>No. of days</h3>
+      <h3>Rent per day</h3>
+      <h3>EC amount</h3>
+      <h3>LC amount</h3>
+      <h3>Other bill</h3>
+      <h3>Total bill</h3>
+      <h3>Cash at hotel</h3>
+      <h3>UPI</h3>
+      <h3>EDC</h3>
+      <h3>App paid amount</h3>
+      <h3>Pending amount</h3>
+      <h3>Booking status</h3>
+    </div>
+  );
+}
